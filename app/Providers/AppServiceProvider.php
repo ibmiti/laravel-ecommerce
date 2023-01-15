93d3009b0c19d631a513a37af88dbebd8870ec80
Dimitri M <ibmiti@gmail.com>
2023-01-15 10:19:41 -0500

    installed the laravel auth scaffold

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+// use illuminate\Suppor\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Do I really need this?
+        // Schema::defaultStringLength(191);
     }
 }
